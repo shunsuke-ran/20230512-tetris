192112bd81bc85d08c450234380173fbb5cc395c
shunsuke-ran <221205076@ccmailg.meijo-u.ac.jp>
2023-05-12 15:44:06 +0900

    スペルミスを修正

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'purple', 'purple', 'perple', 'purple', 'purple', 'purple', 'purple'
+    'purple', 'purple', 'purple', 'purple', 'purple', 'purple', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
